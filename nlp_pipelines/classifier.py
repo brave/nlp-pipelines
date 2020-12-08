a56e931eaaee099c7e12e48555e3e14bc67720c3
pkhaustov <khaustovpavel@gmail.com>
2020-12-08 15:19:24 +0300

    Added binary classification support

--- nlp_pipelines/classifier.py
+++ nlp_pipelines/classifier.py
@@ -1,17 +1,17 @@
-
 from enum import EnumMeta
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import SGDClassifier
 import json
 import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+
 # from . import pipeline_pb2
 class Classifier_Type(EnumMeta):
-    LINEAR = "LINEAR"# linear svm
+    LINEAR = "LINEAR" # linear svm
 
 def build_classifier(classifier_type, reg_param=1.0, **kwargs):
     if classifier_type == Classifier_Type.LINEAR:
-        model = LinearSVC( verbose=1, C=reg_param )
-        return model 
+        model = LinearSVC(verbose=1, C=reg_param)
+        return model
     else:
         raise ValueError('Unknown classifier type')
 
@@ -31,28 +31,40 @@ def jsonify(fp_containing_object,rounding_precision):
     return json.loads(json.dumps(fp_containing_object), parse_float= lambda x:round(float(x), rounding_precision ) )#,  parse_float=lambda x: round(float(x), rounding_precision))))
 
 class Classifier:
-    def __init__(self, classifier_type = Classifier_Type.LINEAR, reg_param = 1.0, **kwargs):
+    def __init__(self, classifier_type=Classifier_Type.LINEAR, reg_param=1.0, **kwargs):
         self.classifier_type = classifier_type
         self.classifier_params = kwargs
-        self.classifier = build_classifier(classifier_type,reg_param=reg_param, **kwargs)
+        self.classifier = build_classifier(classifier_type, reg_param=reg_param, **kwargs)
         self.restored_from_file = False
         self.name_to_labels = {}
         self.label_to_name = {}
-        self.all_labels=[]
-        # self.json_rep=None
-    def train(self, data, labels):
+        self.all_labels = []
+        self.positive_class = None
+        # self.json_rep = None
+
+    def map_names_to_labels(self, labels, positive_class=None):
         uniq_labels = sorted(list(set(labels)))
-        self.name_to_labels={ uniq_label:i for i,uniq_label in enumerate(uniq_labels)}
-        self.label_to_name={i:uniq_label for i, uniq_label in enumerate(uniq_labels)}
-        numeric_labels = []
-        for label in labels:
-            numeric_labels.append(self.name_to_labels[label])
+        if len(uniq_labels) == 2:
+            if positive_class is None:
+                self.positive_class = uniq_labels[1]
+            else:
+                self.positive_class = positive_class
+            if uniq_labels[1] != self.positive_class:
+                uniq_labels[0], uniq_labels[1] = uniq_labels[1], uniq_labels[0]
+        self.name_to_labels = {uniq_label: ind for ind, uniq_label in enumerate(uniq_labels)}
+        self.label_to_name = {ind: uniq_label for ind, uniq_label in enumerate(uniq_labels)}
+
+    def train(self, data, labels, positive_class=None):
+        self.map_names_to_labels(labels, positive_class)
+        numeric_labels = [self.name_to_labels[label] for label in labels]
         numeric_labels = np.array(numeric_labels)
         self.classifier.fit(data, numeric_labels)
+
     def partial_train(self, data, labels, uniq_labels):
-        if len(self.name_to_labels)==0:
-            self.name_to_labels={ uniq_label:i for i,uniq_label in enumerate(uniq_labels)}
-            self.label_to_name={i:uniq_label for i, uniq_label in enumerate(uniq_labels)}
+        # Doesn't support binary classification
+        if len(self.name_to_labels) == 0:
+            self.name_to_labels = {uniq_label: i for i, uniq_label in enumerate(uniq_labels)}
+            self.label_to_name = {i: uniq_label for i, uniq_label in enumerate(uniq_labels)}
             for nl in self.name_to_labels.values():
                 self.all_labels.append(nl)
         numeric_labels = []
@@ -60,17 +72,20 @@ class Classifier:
             numeric_labels.append(self.name_to_labels[label])
         numeric_labels = np.array(numeric_labels)
         self.classifier.partial_fit(data, numeric_labels, classes=self.all_labels)
+
     def predict(self, data):
         if self.restored_from_file:
             return self.predict_from_json(data)
         else:
             return self.predict_from_model(data)
+
     def predict_from_model(self, data):
         preds = self.classifier.predict(data)
         rtn = []
         for pred in preds: 
             rtn.append(self.label_to_name[pred])
         return rtn
+
     def predict_from_json(self, data):
         preds = []
         for c_name, c_weights in self.class_weights.items():
@@ -85,27 +100,36 @@ class Classifier:
         for mc in max_class:
             rtn.append(class_names[mc])
         return rtn
-    def to_json(self,rounding_precision=4):
+
+    def to_json(self, rounding_precision=4):
         rtn = {}
-        rtn['classifier_type']=self.classifier_type
+        rtn['classifier_type'] = self.classifier_type
         classes = []
         for c in self.classifier.classes_:
             classes.append(self.label_to_name[c])
-        rtn['classes'] = classes
+        if len(classes) == 2:
+            rtn['classes'] = [classes[1]]
+        else:
+            rtn['classes'] = classes
         class_weights = {}
-        for i, class_name in enumerate(classes):
-            class_weights[class_name]=self.classifier.coef_[i,:].tolist()
+        # Handling the binary classification case with a single output value
+        if len(classes) == 2:
+            class_weights[classes[1]] = self.classifier.coef_[0, :].tolist()
+        else:
+            for i, class_name in enumerate(classes):
+                class_weights[class_name] = self.classifier.coef_[i, :].tolist()
         rtn['class_weights'] = class_weights
-        rtn['class_weights'] = jsonify(class_weights,rounding_precision) 
+        rtn['class_weights'] = jsonify(class_weights, rounding_precision)
         if 'class_log_prior_' in self.classifier.__dict__:
             rtn['biases'] = jsonify(self.classifier.class_log_prior_.tolist(), rounding_precision)
-        else :
-            zero_priors = [0 for i in range(len(classes))]
+        else:
+            if len(classes) == 2:
+                zero_priors = [0]
+            else:
+                zero_priors = [0] * len(classes)
             rtn['biases'] = jsonify(zero_priors, rounding_precision)
         return rtn
-        
 
-        
     # def to_proto(self):
     #     if self.classifier_type == Classifier_Type.NB:
     #         classes = []
@@ -117,7 +141,6 @@ class Classifier:
     #         class_priors = self.classifier.class_log_prior_
     #         nb = pipeline_pb2.Naive_bayes(classes=classes, vectors = vectors, class_priors = class_priors)
     #         return pipeline_pb2.Classifier(nb=nb)
-
 
 # def classifier_from_proto(classifier_proto):
 #     if classifier_proto.HasField('nb'):
--- nlp_pipelines/nlp_pipeline.py
+++ nlp_pipelines/nlp_pipeline.py
@@ -1,19 +1,20 @@
-from . classifier import Classifier, Classifier_Type
-from enum import Enum
 from datetime import datetime
+from enum import Enum
 import json
 import os
+
+from .classifier import Classifier, Classifier_Type
 
 PIPELINE_VERSION = 1
 
 class Language(Enum):
-    EN = "EN"
-    DE = "DE"
-    FR = "FR"
-    JA = "JA"
-    ES = "ES"
-    PT = "PT"
-    EL = "EL"
+    EN = 'EN'
+    DE = 'DE'
+    FR = 'FR'
+    JA = 'JA'
+    ES = 'ES'
+    PT = 'PT'
+    EL = 'EL'
     AR = 'AR'
     FI = 'FI'
     HE = 'HE'
@@ -51,7 +52,6 @@ Language_to_code = {
     'TL' : 'kcoilhabhhnfdakenmhddnhngngggcmp',
     'TR' : 'kpdcfihnokkbialolpedfamclbdlgopi',
     'ZH' : 'oblfikajhadjnmjiihdchdfdcfehlbpj'
-
 }
 
 def build_manifest(country_string):
@@ -64,56 +64,66 @@ def build_manifest(country_string):
     return json.dumps(manifest)
 
 class NLP_Model:
-    def __init__(self, language, representation=None, 
-                classifier_type=Classifier_Type.LINEAR, version = PIPELINE_VERSION, 
-                classifier = None, reg_param=1.0):
+    def __init__(self,
+                 language,
+                 representation=None,
+                 classifier_type=Classifier_Type.LINEAR,
+                 version=PIPELINE_VERSION,
+                 classifier=None,
+                 reg_param=1.0):
         if language.upper() not in Language.__members__:
             raise ValueError('Unknown language')
         self.version = version
         self.language = language
-        self.representation=representation
+        self.representation = representation
         if classifier is None:
             self.classifier = Classifier(classifier_type=classifier_type, reg_param=reg_param)
         else:
             self.classifier = classifier
+
     def apply_transforms(self, data):
         tmp = data
         for transform in self.representation:
-            tmp = transform.apply( tmp )
+            tmp = transform.apply(tmp)
         return tmp
-    def train(self, data, labels):
+
+    def train(self, data, labels, positive_class=None):
         rep = data
         if self.representation is not None:
             rep = self.apply_transforms(data)
-        self.classifier.train(rep,labels)
-    def partial_train(self, data, labels,uniq_labels):
+        self.classifier.train(rep, labels, positive_class)
+
+    def partial_train(self, data, labels, uniq_labels):
         rep = data
         if self.representation is not None:
             rep = self.apply_transforms(data)
-        self.classifier.partial_train(rep,labels,uniq_labels)
+        self.classifier.partial_train(rep,labels, uniq_labels)
+
     def predict(self, data):
         rep = data
         if self.representation is not None:
             rep = self.apply_transforms(data)
         return self.classifier.predict(rep)
+
     def to_json(self):
-        rtn = {"locale" : self.language}
-        rtn["version"] = PIPELINE_VERSION
-        rtn["timestamp"] = str( datetime.utcnow() )
-        rtn["transformations"] = []
+        rtn = {'locale': self.language}
+        rtn['version'] = PIPELINE_VERSION
+        rtn['timestamp'] = str(datetime.utcnow())
+        rtn['transformations'] = []
         for t in self.representation:
-            rtn["transformations"].append(t.to_json()) 
-        rtn["classifier"] = self.classifier.to_json()
+            rtn['transformations'].append(t.to_json())
+        rtn['classifier'] = self.classifier.to_json()
         return rtn
+
     def save(self, output_dir):
         country_string = Language_to_code[self.language]
         tmp = self.to_json()
         manifest = build_manifest(country_string)
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
-        with open( os.path.join(output_dir, country_string) , 'w') as f:
+        with open(os.path.join(output_dir, country_string), 'w') as f:
             f.write(json.dumps(tmp))
-        with open( os.path.join(output_dir, 'manifest.json') , 'w') as f:
+        with open(os.path.join(output_dir, 'manifest.json'), 'w') as f:
             f.write(manifest)
 
 def load_model(filename):
